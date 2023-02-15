8e340bd8964bfcb3c925cf766e69e4ed6585549b
Dhruva-Patil1509 <dhruvasp@gmail.com>
2023-02-15 20:29:16 +0530

    Initial commit

---
+++ src/test/java/com/grade_submission/gradesubmission/GradeSubmissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grade_submission.gradesubmission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradeSubmissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
